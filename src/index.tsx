63ca3a25929181cdd21860bd800e335f65dd924a
Max Johnson <max.johnson@softwire.com>
2024-04-30 10:48:56 +0100

    Use dark theme (because nasa)

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+document.documentElement.dataset.bsTheme = "dark";
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
